768e24d05d7e093292e208742b80a2c2e861814e
Kunal Kamble <kkunal785@gmail.com>
2026-08-05 04:39:56 +0530

    Added a comment to line 2

--- band_name_generator.py
+++ band_name_generator.py
@@ -1,4 +1,4 @@
 print("Welcome to the band name generator!") # Test commit after PAT setup
-city=input("What's the name of the city you grew up in?\n")
+city=input("What's the name of the city you grew up in?\n") #PushPractise 
 pet=input("What's your pet's name?\n")
 print("Your band name could be " + city +" "+ pet + ".")
