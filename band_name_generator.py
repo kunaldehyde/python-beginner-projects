42b0b3df8a08e4af46083d413d131b26bddb64ad
Kunal Kamble <kkunal785@gmail.com>
2026-08-05 19:36:29 +0530

    Updated the code with proper spacing for improved readability

--- band_name_generator.py
+++ band_name_generator.py
@@ -1,4 +1,4 @@
-print("Welcome to the band name generator!") # Test commit after PAT setup
-city=input("What's the name of the city you grew up in?\n") #PushPractise 
-pet=input("What's your pet's name?\n")
-print("Your band name could be " + city +" "+ pet + ".")
+print("Welcome to the band name generator")
+city = input("Please enter the city you grew up in:\n")
+pet = input("Please enter your pet's name:\n")
+print("Your band name could be " + city + " " + pet + ".")
